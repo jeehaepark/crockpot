40519a2ebbd85e8df293053027988e9d8595e962
James Youn <jamesyoun710@gmail.com>
2016-04-22 17:32:02 -0700

    Scaffold files & code

--- specs/specs.js
+++ specs/specs.js
@@ -1,11 +1,20 @@
 var expect = require('chai').expect;
 var romanToDecimal = require('./../src/conversion');
+
 var testParams = require('./testList')
 
-describe('Roman Numeral# ', function(){
-  testParams.forEach(function(param){
-    it('translate ' + param.numeral + ' to decimal', function(){
-      expect(romanToDecimal(param.numeral)).to.equal(param.decimal);
-    })
-  })
+describe('English to Roman numeral# ', function(){
+  // testParams.forEach(function(param){
+  //   it('translate ' + param.numeral + ' to decimal', function(){
+  //     expect(romanToDecimal(param.numeral)).to.equal(param.decimal);
+  //   })
+  // })
+});
+
+describe('Roman numeral to English# ', function(){
+  // testParams.forEach(function(param){
+  //   it('translate ' + param.numeral + ' to decimal', function(){
+  //     expect(romanToDecimal(param.numeral)).to.equal(param.decimal);
+  //   })
+  // })
 });
